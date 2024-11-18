9c39c452ac60dcc1a6557f3f061c6a2d1dc75ccd
Dev Michoma <codesplint151@gmail.com>
2024-11-18 16:31:14 +0300

    Styling the add to do form

--- Task/src/AddTodoForm.tsx
+++ Task/src/AddTodoForm.tsx
@@ -3,9 +3,10 @@ export default function AddTodoForm(){
     <form>
         <h2>Add a todo</h2>
         <input type="text"
-        className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full"
+        className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] px-[16px] block w-full"
         />
-        <button type="submit">Add to List</button>
+        <button type="submit" className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
+            Add to List</button>
     </form>
     )
   }
--- Task/src/Sidebar.tsx
+++ Task/src/Sidebar.tsx
@@ -2,7 +2,7 @@ import AddTodoForm from "./AddTodoForm";
 
 export default function Sidebar(){
     return(
-        <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08] px-[25px]">
+        <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08] px-[25px] pt-[18px] pb-[28px]">
                <AddTodoForm/>
         </section>
       
