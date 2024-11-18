a4971d046a38c0d6380258d1223ea2af2f796167
Dev Michoma <codesplint151@gmail.com>
2024-11-18 12:28:48 +0300

    Small code refactoring

--- Task/src/Header.tsx
+++ Task/src/Header.tsx
@@ -1,5 +1,11 @@
 export default function Header(){
     return(
-        <header className="col-[1/3] row-[1/2] bg-[#fbf5ed]" ></header>
+        <header className="col-[1/3] flex justify-between items-center row-[1/2]  px-[28px] bg-[#fbf5ed] border-b border-black/[0.08]" >
+            
+
+            <p>
+                <b>0</b> / 0 todos completed
+            </p>
+        </header>
     )
   }
---
+++ Task/src/Logo.tsx
@@ -0,0 +1,6 @@
+export default function BackgroundHeading(){
+    return(
+        <img src= "https://bytegrad.com/course-assets/react-nextjs/dots.png"/>
+    
+    )
+  }
