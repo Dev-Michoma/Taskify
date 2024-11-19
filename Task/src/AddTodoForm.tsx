4edc1a6fcaeb89ea8a135f8ba39509ce0474a9bd
Dev Michoma <codesplint151@gmail.com>
2024-11-19 17:25:44 +0300

    Contrlled input functionality

--- Task/src/AddTodoForm.tsx
+++ Task/src/AddTodoForm.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function AddTodoForm({todos ,setTodos}){
+
+   const [todoText ,setTodoText] = useState("")
     return(
     <form onSubmit={
       (e) =>{
         e.preventDefault();
+
+        setTodos(prev => [
+          ...prev,
+          {
+            id:prev.length + 1,
+            text: todoText,
+            isCompleted : false,
+          }
+        ]);
+        setTodoText("");
       }
-    }>
+    } 
+    >
+
+
         <h2>Add a todo</h2>
         <input type="text"
         className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] px-[16px] block w-full"
-        />
+        value= {todoText} 
+        onChange = {(e) => {
+          setTodoText(e.target.value);
+        }}/>
+    
        <Button>Add to List</Button>
     </form>
     )
