9bca39266c95d4859bee9fe4a14f15c0b570c1d6
Dev Michoma <codesplint151@gmail.com>
2024-11-19 16:59:08 +0300

    Add Todo  functionality

--- Task/src/AddTodoForm.tsx
+++ Task/src/AddTodoForm.tsx
@@ -2,7 +2,11 @@ import Button from "./Button";
 
 export default function AddTodoForm({todos ,setTodos}){
     return(
-    <form>
+    <form onSubmit={
+      (e) =>{
+        e.preventDefault();
+      }
+    }>
         <h2>Add a todo</h2>
         <input type="text"
         className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] px-[16px] block w-full"
