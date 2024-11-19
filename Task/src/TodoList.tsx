ed3d8ccc286c76e1e82c40cf6e7800934dfc4f8b
Dev Michoma <codesplint151@gmail.com>
2024-11-19 12:22:57 +0300

    Initializing the usestate hook

--- Task/src/TodoList.tsx
+++ Task/src/TodoList.tsx
@@ -1,30 +1,33 @@
 import DeleteButton from "./DeleteButton"
-
+import { useState } from "react"
 export default  function TodoList() {
-    const initialTodos = [
-        {
-            text : "Buy Groceries",
-            isCompleted : true,
-        },  
-        {
-            text : "Write Typescript",
-            isCompleted : true,
-        },  
-        {
-            text : "Learn Crypto",
-            isCompleted : true,
-        },  
-        {
-            text : "Linkedin forensics",
-            isCompleted : false,
-        },   
-    
-    ]
+   
+   const [todos ,setTodos]= useState(
+        [
+            {
+                text : "Buy Groceries",
+                isCompleted : true,
+            },  
+            {
+                text : "Write Typescript",
+                isCompleted : true,
+            },  
+            {
+                text : "Learn Crypto",
+                isCompleted : true,
+            },  
+            {
+                text : "Linkedin forensics",
+                isCompleted : false,
+            },   
+        
+        ]
+    )
     return(
         <ul>
 
             {
-                initialTodos.map(todo =>(
+                todos.map(todo =>(
                     <li key={todo.text} className="flex justify-between items-center px-8 h-[50px]
                     text-[14px] cursor-pointer border-b border-black/[8%]">
                         <span className={`${todo.isCompleted ? 'line-through text-[#ccc]' : ''}`}>{todo.text}</span>
